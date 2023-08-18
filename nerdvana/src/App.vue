0d924f0506909dbb417fe160e7b8203dce6d98a4
Marcos Oliveira <oliveira.marcos@engenharia.ufjf.br>
2023-08-17 22:39:52 -0300

    Create first connection to nerdvana api

--- nerdvana/src/App.vue
+++ nerdvana/src/App.vue
@@ -1,47 +1,43 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <input class="city-input" type="text" v-model="gameInput" placeholder="E.g., God of War, Elden Ring">
+  <button class="search-game-btn" @click.prevent="fetchGames()"> Search </button>
+  <li v-for="game in gamesList">{{ game.name }} - {{ game.release }}</li>
+  <h4>Recomendações</h4>
+  <li v-for="game in recommendedGames">{{ game.name }} - {{ game.release }}</li>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
+<script>
+export default {
+  data() {
+    return {
+      gameInput: "",
+      gamesList: [],
+      recommendedGames: []
+    }
+  },
+  methods: {
+    async fetchGames() {
+      if (!this.gameInput) {
+        return null;
+      }
+      // Get games
+      const GAME_URL = `http://127.0.0.1:8000/api/games?name_contains=${this.gameInput}&limit=10&offset=0`;
+      console.log(GAME_URL);
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
+      const gamesResponse = await fetch(GAME_URL);
+      const gamesData = await gamesResponse.json();
+      this.gamesList = gamesData;
+      console.log(this.gamesList);
+      
+      const firstGame = this.gamesList[0];
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+      const RECOMMENDER_URL = `http://127.0.0.1:8000/api/recommender?game_id=${firstGame.id}&console_id=3&number_of_recommendations=10`;
+
+      // Get recommendations
+      const gamesRecommender = await fetch(RECOMMENDER_URL);
+      const recommendedData = await gamesRecommender.json();
+      this.recommendedGames = recommendedData;
+    }
   }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
 }
-</style>
+</script>
