f732ba594fab5d6d4e7ce6667a9e6ff6df71ba91
Marcos Oliveira <oliveira.marcos@engenharia.ufjf.br>
2023-11-30 18:40:01 -0300

    add game list in home

--- nerdvana/src/views/Home.vue
+++ nerdvana/src/views/Home.vue
@@ -8,6 +8,24 @@
         </div>
     </div>
 </div>
+<div class="container">
+  <div class="row mt-3">
+  <h6 class="text-info"> | Melhores Jogos</h6>
+  <div v-for="bestGame in bestGames" class="col-md-1" style="width: 13.33%; flex: 0 0 13.33%;max-width: 13.33%;">
+    <img class="rounded border img-fluid img-sm" :src="bestGame.game_cover_link" alt="">
+    <div class="small text-white">
+      {{ bestGame.name }}
+    </div>
+  </div>
+  <h6 class="text-info"> | Jogos mais buscados</h6>
+  <div v-for="bestGame in bestGames" class="col-md-1" style="width: 13.33%; flex: 0 0 13.33%;max-width: 13.33%;">
+    <img class="rounded border img-fluid img-sm" :src="bestGame.game_cover_link" alt="">
+    <div class="small text-white">
+      {{ bestGame.name }}
+    </div>
+  </div>
+</div>
+</div>
 </template>
 
 <script>
@@ -18,6 +36,22 @@
     components: {
       Navbar,
       SearchBar,
+    },
+    data() {
+      return {
+        bestGames: {}
+      }
+    },
+    created: async function(){
+        this.bestGames = await this.getBestGames();
+    },
+    methods: {
+        async getBestGames() {
+            const GET_BEST_GAMES = `http://127.0.0.1:8000/api/games?top_games=True`;
+            const bestGamesResponse = await fetch(GET_BEST_GAMES);
+            const bestGamesData = await bestGamesResponse.json();
+            return bestGamesData
+        }
     },
 }
 </script>
